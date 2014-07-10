b22e2c867bc57ab94926edf903b6b661080c7173
edwardridge <edwardridge@Edwards-MacBook-Air.local>
2014-07-10 22:49:22 +0100

    Added UnitTests project

---
+++ UnitTests/DeckTests.cs
@@ -0,0 +1,16 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace UnitTests
+{
+	[TestFixture]
+	public class DeckTests
+	{
+		[Test]
+		public void TestCase ()
+		{
+			Assert.True (true);
+		}
+	}
+}
+
