bacdf503d4a9f15dcb3a7032eaa96eb2ee47bda7
edwardridge <edwardridge@Edwards-MacBook-Air.local>
2014-07-13 22:08:24 +0100

    Refactored stubrandomiser

--- UnitTests/StubRandomiser.cs
+++ UnitTests/StubRandomiser.cs
@@ -1,22 +1,42 @@
 ﻿using System;
 using Palace;
 using System.Collections.Generic;
+using System.Linq;
 
 namespace UnitTests
 {
 	public class StubRandomiser : IShuffler
 	{
-		public StubRandomiser ()
+		int[] cardOrder;
+
+		public StubRandomiser () {
+			this.cardOrder = GetSequentialOrder ();
+		}
+
+		public StubRandomiser (int[] cardOrder)
 		{
+			this.cardOrder = cardOrder;
 		}
 
 		public ICollection<Card> ShuffleCards (ICollection<Card> preShuffledDeck)
 		{
 			List<Card> cards = new List<Card> ();
-			for (int i = 0; i < preShuffledDeck.Count; i++) {
-				cards.Add (new Card (i));
+			var newOrder = cardOrder.Where (i=>i < preShuffledDeck.Count);
+
+			foreach(int order in newOrder){
+				cards.Add(preShuffledDeck.ToArray()[order]);
 			}
 			return cards;
+		}
+
+		public int[] GetSequentialOrder ()
+		{
+			List<int> order = new List<int> ();
+			for (int i = 0; i < 52; i++) {
+				order.Add (i);
+			}
+
+			return order.ToArray ();
 		}
 	}
 }
