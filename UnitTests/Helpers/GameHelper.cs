a33c8e9e2c1ee072871dc50970d456be872a6f7c
edwardridge <edwardridge@Edwards-MacBook-Air.local>
2014-09-04 00:36:04 +0100

    Removed unused code

--- UnitTests/Helpers/GameHelper.cs
+++ UnitTests/Helpers/GameHelper.cs
@@ -7,16 +7,6 @@ namespace UnitTests
 {
 	public static class GameHelper
 	{
-		public static Game CreateGameWithPlayers(ICollection<IPlayer> players){
-
-
-			var randomiser = new NonShuffler ();
-			var deck = new Deck (randomiser);
-			var game = new Game (players, deck);
-			game.Setup ();
-
-			return game;
-		}
 
 		public static void StartGameForTest(this Game game, ICollection<IPlayer> players, IList<IList<int>> cards){
 			game.Setup ();
