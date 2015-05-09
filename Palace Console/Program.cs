4ba1407913485e48d9185d1a9776d203b21d41b0
Edward Ridge <edward@abaresearch.co.uk>
2015-05-09 18:56:13 +0100

    Added console project for testing

---
+++ Palace Console/Program.cs
@@ -0,0 +1,20 @@
+﻿namespace Palace_Console
+{
+    using System.Collections.Generic;
+
+    using Palace;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var ruleForCardsByValue = new Dictionary<CardValue, RuleForCard>();
+            ruleForCardsByValue.Add(CardValue.Seven, RuleForCard.LowerThan);
+            ruleForCardsByValue.Add(CardValue.Ten, RuleForCard.Burn);
+            ruleForCardsByValue.Add(CardValue.Two, RuleForCard.Reset);
+            ruleForCardsByValue.Add(CardValue.Eight, RuleForCard.SeeThrough);
+            ruleForCardsByValue.Add(CardValue.Jack, RuleForCard.SkipPlayer);
+            ruleForCardsByValue.Add(CardValue.Ace, RuleForCard.ReverseOrderOfPlay);
+        }
+    }
+}
